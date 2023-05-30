3daa9d387de5a24f1ecb03804fa25b5f0bca756f
Juhyeok Kang <kangju2000@naver.com>
2023-05-30 14:03:32 +0900

    [level 2] Title: N개의 최소공배수, Time: 0.13 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
+++ 프로그래머스/lv2/12953. N개의 최소공배수/N개의 최소공배수.js
@@ -1,35 +1,6 @@
-/**
-4 6 18
-
-2 / 2 3 9
-3 / 2 1 3
-
-
-*/
 function solution(arr) {
-    let result = 1;
-    let i = 2;
-    while(i <= arr.sort((a,b)=>b-a)[0]) {
-        let cnt = 0;
-        
-        const newArr = arr.reduce((acc,cur) => {
-                if(cur % i === 0) {
-                    cnt += 1;
-                    acc.push(cur/i);
-                }
-                else acc.push(cur);
-                return acc;
-            }, []);
-        
-        if(cnt > 1) {
-            arr = newArr;
-            result *= i;
-            i = 2;
-        }
-        else {
-            i += 1;
-        }
-    }
+    const gcd = (a,b) => a%b ? gcd(b, a%b) : b;
+    const lcm = (a,b) => a*b / gcd(a,b);
     
-    return result * arr.reduce((acc,cur)=>acc*cur, 1);
+    return arr.reduce(lcm);
 }
