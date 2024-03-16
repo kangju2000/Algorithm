fcf89719ec164248d85abb25fd6812b86f856ab7
Juhyeok Kang <kangju2000@naver.com>
2024-03-17 01:53:03 +0900

    [Silver II] Title: 마인크래프트, Time: 608 ms, Memory: 39676 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.js
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.js
@@ -1,52 +1,38 @@
-function solution(input) {
-  const [n, m, b] = input[0].trim().split(" ").map(Number);
-  const blockList = input
-    .slice(1)
-    .map((line) => line.trim().split(" ").map(Number))
-    .flat()
-    .sort((a, b) => b - a);
-  let result; // [time, height]
-  for (
-    let goalHeight = blockList[0];
-    goalHeight >= blockList[blockList.length - 1];
-    goalHeight--
-  ) {
-    let time = 0;
-    let remainBlock = b;
-    for (const blockHeight of blockList) {
-      const diff = goalHeight - blockHeight;
-      if (diff < 0) {
-        time += 2 * Math.abs(diff);
-        remainBlock += Math.abs(diff);
-      }
-      if (diff > 0) {
-        time += Math.abs(diff);
-        remainBlock -= Math.abs(diff);
-        if (remainBlock < 0) {
-          break;
-        }
-      }
-    }
+const fs = require("fs");
+const input = fs.readFileSync("/dev/stdin", "utf8").trim().split("\n");
 
-    if (remainBlock >= 0 && (result === undefined || time < result[0])) {
-      result = [time, goalHeight];
+const [n, m, b] = input[0].trim().split(" ").map(Number);
+const blockList = input
+  .slice(1)
+  .map((line) => line.trim().split(" ").map(Number))
+  .flat()
+  .sort((a, b) => b - a);
+let result; // [time, height]
+for (
+  let goalHeight = blockList[0];
+  goalHeight >= blockList[blockList.length - 1];
+  goalHeight--
+) {
+  let time = 0;
+  let remainBlock = b;
+  for (const blockHeight of blockList) {
+    const diff = goalHeight - blockHeight;
+    if (diff < 0) {
+      time += 2 * Math.abs(diff);
+      remainBlock += Math.abs(diff);
+    }
+    if (diff > 0) {
+      time += Math.abs(diff);
+      remainBlock -= Math.abs(diff);
+      if (remainBlock < 0) {
+        break;
+      }
     }
   }
 
-  console.log(result.join(" "));
+  if (remainBlock >= 0 && (result === undefined || time < result[0])) {
+    result = [time, goalHeight];
+  }
 }
 
-const readline = require("readline");
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const input = [];
-
-rl.on("line", function (line) {
-  input.push(line);
-}).on("close", function () {
-  solution(input);
-  process.exit();
-});
+console.log(result.join(" "));
