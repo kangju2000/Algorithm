03d221f0d630d730554072d88c9627d4858dea42
Juhyeok Kang <kangju2000@gmail.com>
2024-08-31 16:41:54 +0900

    Time: 57 ms (25.84%), Space: 49.1 MB (19.47%) - LeetHub

---
+++ LeetCode/2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function (val) {
+  return {
+    toBe: (a) => {
+      if (val !== a) {
+        throw new Error("Not Equal")
+      }
+
+      return true
+    },
+    notToBe: (a) => {
+      if (val === a) {
+        throw new Error("Equal")
+      }
+
+      return true
+    }
+  }
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
