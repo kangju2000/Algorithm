e6fa77c90fbe4bb8cbba94557a2fb256fc30bddf
Juhyeok Kang <kangju2000@gmail.com>
2024-09-01 17:34:02 +0900

    Time: 55 ms (90.5%), Space: 52 MB (69.72%) - LeetHub

--- LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ LeetCode/0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,16 +3,16 @@
  * @return {number}
  */
 var removeDuplicates = function (nums) {
-  const uniqueArr = [];
-  let index = 0;
-  for (let i = 0; i < nums.length; i++) {
-    if (uniqueArr[uniqueArr.length - 1] === nums[i]) {
-      continue;
+  if (nums.length === 0) return 0;
+
+  let uniqueIndex = 0;
+
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] !== nums[uniqueIndex]) {
+      uniqueIndex++;
+      nums[uniqueIndex] = nums[i];
     }
-    uniqueArr.push(nums[i]);
-    nums[index] = nums[i];
-    index++;
   }
 
-  return uniqueArr.length;
+  return uniqueIndex + 1;
 };
