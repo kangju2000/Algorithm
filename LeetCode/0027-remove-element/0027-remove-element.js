8e18e2545258e1c9e777e37b783d843b6682c5ec
Juhyeok Kang <kangju2000@gmail.com>
2024-09-05 01:24:29 +0900

    Time: 69 ms (5.64%), Space: 50.7 MB (5.14%) - LeetHub

---
+++ LeetCode/0027-remove-element/0027-remove-element.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number[]} nums
+ * @param {number} val
+ * @return {number}
+ */
+var removeElement = function (nums, val) {
+    let left = 0;
+    let right = nums.length - 1;
+
+    if (nums.length === 1 && nums[left] !== val) {
+        return 1;
+    }
+
+    while (left <= right) {
+        if (nums[left] === val) {
+            while (nums[right] === val) right--;
+            if (left > right) {
+                break;
+            }
+            nums[left] = nums[right];
+            nums[right] = val;
+        } else {
+            left++;
+        }
+    }
+
+    console.log({ left, right, nums })
+
+    return left;
+};
