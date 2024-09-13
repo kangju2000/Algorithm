ffe6388820146d4fdd594d9a40b965d5386e9a6d
kangju2000 <kangju2000@naver.com>
2024-09-14 02:19:52 +0900

    template

--- template.js
+++ template.js
@@ -1,17 +1,31 @@
-function solution(input) {}
-
 const readline = require("readline");
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-const input = [];
+let input = [];
+let lineCount = 0;
 
-rl.on("line", function (line) {
-  // input = line.trim().split(" ").map(Number);
-  // input.push(line.trim());
-}).on("close", function () {
-  solution(input);
+function processInput(line) {
+  if (lineCount === 0) {
+    const [N, M] = line.split(" ").map(Number);
+    input.push({ N, M });
+  } else {
+    input.push(line.split(" ").map(Number));
+  }
+  lineCount++;
+}
+
+rl.on("line", processInput).on("close", () => {
+  solution();
   process.exit();
 });
+
+function solution() {
+  const { N, M } = input[0];
+
+  // 여기에 문제 해결 로직을 구현합니다.
+
+  console.log("결과를 여기에 출력하세요.");
+}
